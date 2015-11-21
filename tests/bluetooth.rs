1612b006961c8625949898a7d37a3b6010bb1821
Richard Protasov <richardprotasov@gmail.com>
2015-11-21 02:13:48 -0500

    Removes an unnecessary test

--- tests/bluetooth.rs
+++ tests/bluetooth.rs
@@ -1,8 +1,1 @@
 extern crate bluetooth;
-
-use bluetooth::central::manager as BCCentralManager;
-
-#[test]
-fn stuff_is_connected() {
-    assert_eq!("Hello, world".to_string(), BCCentralManager::hello_world());
-}
