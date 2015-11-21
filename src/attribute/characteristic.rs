902656bd78d5465d9f927f22059ae3e63da946eb
Richard Protasov <richardprotasov@gmail.com>
2015-11-21 02:10:02 -0500

    Removes constraining placeholder structs for abstracting attributes
    Additionally, significant structures (I.E. UUID, Service,
    Characteristic) are placed directly into src folder.

--- src/attribute/characteristic.rs
+++
@@ -1,7 +0,0 @@
-struct Characteristic {
-    service: Service,
-    value: Data,
-    descriptors: Vec<Descriptors>,
-    properties: CharacteristicProperties,
-    is_notifying: bool,
-}
--- src/uuid.rs
+++ src/uuid.rs
@@ -1,3 +1,3 @@
 pub enum UUID {
-    Device,
+    Example,
 }
