087891dd6df12b4a7640e7dc96e76773e896f8bf
Richard Protasov <richardprotasov@gmail.com>
2015-11-21 23:26:15 -0500

    Implements several common traits for UUID
    UUID still follows the pattern of using custom UUIDS. Not sure as to
    whether this set up can also provide support for Bluetooth Base UUIDs.

--- src/uuid.rs
+++ src/uuid.rs
@@ -1,3 +1,27 @@
+use std::fmt::{Debug, Formatter, Result};
+
 pub enum UUID {
-    Example,
+    Custom(u16),
+}
+
+impl UUID {
+    pub fn as_hex(hex_string: &'static str) -> UUID {
+        return UUID::Custom(0xff)
+    }
+
+    pub fn id(&self) -> u16 {
+        match *self { UUID::Custom(ref id) => *id, }
+    }
+}
+
+impl PartialEq for UUID {
+    fn eq(&self, other: &UUID) -> bool {
+        self.id() == other.id()
+    }
+}
+
+impl Debug for UUID {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "UUID:{}", self.id())
+    }
 }
