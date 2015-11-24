3a72a131c8d07ff1d289b6d063901c7445b91721
Richard Protasov <richardprotasov@gmail.com>
2015-11-24 04:02:50 -0500

    Defines Bluetooth standard UUIDs for services and characertistics

--- src/uuid.rs
+++ src/uuid.rs
@@ -1,27 +1,35 @@
 use std::fmt::{Debug, Formatter, Result};
 
 pub enum UUID {
-    Custom(u16),
+    Custom(u32),
 }
 
 impl UUID {
     pub fn as_hex(hex_string: &'static str) -> UUID {
+        /*
+         * Users can enter a hexadeciaml
+         * strings and will convert the given value
+         * to its numerical representation.
+         *
+         * Returns a UUID::Custom with the
+         * given hex string.
+        */
         return UUID::Custom(0xff)
     }
 
-    pub fn id(&self) -> u16 {
-        match *self { UUID::Custom(ref id) => *id, }
+    pub fn to_hex(&self) -> u32 {
+        match *self { UUID::Custom(ref hex) => *hex, }
     }
 }
 
 impl PartialEq for UUID {
     fn eq(&self, other: &UUID) -> bool {
-        self.id() == other.id()
+        self.to_hex() == other.to_hex()
     }
 }
 
 impl Debug for UUID {
     fn fmt(&self, f: &mut Formatter) -> Result {
-        write!(f, "UUID:{}", self.id())
+        write!(f, "UUID:{}", self.to_hex())
     }
 }
