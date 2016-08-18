39fd3535c19c1e8185bc38e3cb3581d44027947c
Richard Protasov <richardprotasov@gmail.com>
2016-08-17 21:53:51 -0400

    Improve path definitions in kawaii

--- libs/kawaii/src/lib.rs
+++ libs/kawaii/src/lib.rs
@@ -1,23 +1,132 @@
-#![feature(trace_macros)]
+#![feature(trace_macros, non_ascii_idents)]
 
 extern crate rustless;
 extern crate iron;
 extern crate curl;
 
-pub use iron::Iron;
-pub use rustless::{
-    Application, Api, Nesting, Versioning
+pub use rustless::
+{
+    Application,
+    Api,
+    Nesting,
+    Versioning
 };
+pub use rustless::framework::client::ClientResult;
+pub use rustless::Client as RustlessClient;
+pub use rustless::json::JsonValue;
 
+pub type Response<'a>   = ClientResult<'a>;
+pub type Parameters<'a> = &'a JsonValue;
+pub type Client<'a>     = RustlessClient<'a>;
+
+#[macro_export]
 macro_rules! 可愛い {
-    ($a:ident) => {
-        let api = Api::build(|api| {
-            api.prefix("api");
-            api.version("v1", Versioning::AcceptHeader("chat"));
-        });
+    ( $action:ident ( $n:ident / $($rest:tt)* ) -> $func:expr; $($next:tt)* )
+        => {{
+            let api = Api::build(|api| {
+                api.prefix("api");
+                api.version("v1", Versioning::AcceptHeader("chat"));
 
-        let $a = Application::new(api);
-    };
+                可愛い!(next api, $action ( $n / $($rest)* ) -> $func; $($next)* );
+            });
+
+            Application::new(api)
+        }};
+
+    ( $action:ident ( $n:ident : $m:ident / $($rest:tt)* ) -> $func:expr; $($next:tt)* )
+        => {{
+            let api = Api::build(|api| {
+                api.prefix("api");
+                api.version("v1", Versioning::AcceptHeader("chat"));
+
+                可愛い!(next api, $action ( $n : $m / $($rest)* ) -> $func; $($next)* );
+            });
+
+            Application::new(api)
+        }};
+
+    ( $action:ident ( $n:ident ) -> $func:expr; $($next:tt)* )
+        => {{
+            let api = Api::build(|api| {
+                api.prefix("api");
+                api.version("v1", Versioning::AcceptHeader("chat"));
+
+                可愛い!(next api, $action ( $n ) -> $func; $($next)* );
+            });
+
+            Application::new(api)
+        }};
+
+    ( $action:ident ( $n:ident : $m:ident ) -> $func:expr; $($next:tt)* )
+        => {{
+            let api = Api::build(|api| {
+                api.prefix("api");
+                api.version("v1", Versioning::AcceptHeader("chat"));
+
+                可愛い!(next api, $action ( $n : $m ) -> $func; $($next)* );
+            });
+
+            Application::new(api)
+        }};
+
+    ( next $api:ident, $action:ident ( $n:ident / $($rest:tt)* ) -> $func:expr; $($next:tt)* )
+        => {{
+            use std::fmt::Write;
+
+            let mut route = String::new();
+            可愛い!(route route, $n / $($rest)*);
+            可愛い!(scaffold $api, $action &route[..], $func);
+            可愛い!(next $api, $($next)*);
+        }};
+
+    ( next $api:ident, $action:ident ( $n:ident : $m:ident / $($rest:tt)* ) -> $func:expr; $($next:tt)* )
+        => {{
+            use std::fmt::Write;
+
+            let mut route = String::new();
+            可愛い!(route route, $n : $m / $($rest)*);
+            可愛い!(scaffold $api, $action &route[..], $func);
+            可愛い!(next $api, $($next)*);
+        }};
+
+    ( next $api:ident, $action:ident ( $n:ident ) -> $func:expr; $($next:tt)* )
+        => {{
+            可愛い!(scaffold $api, $action stringify!($n), $func);
+            可愛い!(next $api, $($next)*);
+        }};
+
+    ( next api:ident, $action:ident ( $n:ident : $m:ident ) -> $func:expr; $($next:tt)* )
+        => {{
+            可愛い!(scaffold $api, $action stringify!($n), $func);
+            可愛い!(next $api, $($next)*);
+        }};
+
+    ( next $api:expr, )
+        => ();
+
+    ( route $buf:expr, $n:ident / $($rest:tt)* )
+        => (let _ = write!($buf, "{}/", stringify!($n)); 可愛い!(route $buf, $($rest)*) );
+
+    ( route $buf:expr, $n:ident : $m:ident / $($rest:tt)* )
+        => (let _ = write!($buf, ":{}/", stringify!($n)); 可愛い!(route $buf, $($rest)*) );
+
+    ( route $buf:expr, $n:ident )
+        => (let _ = write!($buf, "{}", stringify!($n)); );
+
+    ( route $buf:expr, $n:ident : $m:ident )
+        => (let _ = write!($buf, ":{}", stringify!($n)); );
+
+    // ( scaffold $ns:expr; $action:ident $route:expr ( $($n:ident : $t:ident)* ) -> $func:expr )
+    ( scaffold $ns:expr, $action:ident $route:expr, $func:expr )
+        => ($ns.$action($route, |ep| ep.handle(|c, p| $func(c, p))));
+        // => (
+        //     $ns.$action($route, |ep| {
+        //         ep.params(|params| $( params.req_typed($n, json_dsl::$t()); )* );
+        //         ep.handle(|c, p| $func(c, p))
+        //     })
+        // );
+
+    () => ();
 }
 
 macro_rules! client_get {
@@ -94,13 +203,14 @@ macro_rules! client_post {
         use curl::easy::Easy;
         use std::io::Read;
 
-        let mut handle   = Easy::new();
+        let mut handle = Easy::new();
 
         client_post!(handle, url => $u);
         client_post!(handle, header => $h);
         client_post!(handle, body => $b);
 
         let mut payload = $b.as_bytes();
+
         {
             let mut transfer = handle.transfer();
 
@@ -125,13 +235,12 @@ mod test {
     use super::*;
     use std::thread;
     use std::net::SocketAddr;
+    use iron::Iron;
 
     struct Mock {
         application: Application,
         url: SocketAddr
     }
-
-    struct Controller;
 
     impl Mock {
         fn new(app: Application, url: SocketAddr) -> Self {
@@ -150,14 +259,11 @@ mod test {
         }
     }
 
-    impl Controller {
-        fn emoji(client: Client) -> Response {
-            client.text("🙈")
-        }
-
-        fn echo(client: Client, msg: String) -> Response {
-            client.text(msg)
-        }
+    macro_rules! start {
+        ($port:expr => $app:expr) => {
+            let _ = Mock::new($app, socket($port))
+                .start();
+        };
     }
 
     fn socket(url: &str) -> SocketAddr {
@@ -165,11 +271,28 @@ mod test {
             .expect("Unable to parse socket address")
     }
 
+    fn emoji<'a>(client: Client<'a>, _params: Parameters) -> Response<'a> {
+        client.text("🙈".to_string())
+    }
+
+    fn echo<'a>(client: Client<'a>, params: Parameters) -> Response<'a> {
+        println!("working!");
+        let msg = match params.find("msg") {
+            Some(m) => m.as_str().unwrap(),
+            None    => "😅"
+        };
+
+        client.text(msg.to_string())
+    }
+
     #[test]
     fn simple_get() {
-        可愛い!(application, get(emoji) -> Controller.emoji);
-        let url = socket("127.0.0.1:8000");
-        let _ = Mock::new(application, url).start();
+        let application =
+            可愛い!(
+                get (emoji) -> emoji;
+            );
+
+        start!("127.0.0.1:8000" => application);
 
         let mut buffer = Vec::new();
 
@@ -185,16 +308,20 @@ mod test {
 
     #[test]
     fn simple_post() {
-        可愛い!(application, post(echo/:msg) -> Controller.echo);
-        let url = socket("127.0.0.1:8001");
-        let _ = Mock::new(application, url).start();
+        trace_macros!(true);
+        let application =
+            可愛い!(
+                post (echo/msg:string) -> echo;
+            );
+        trace_macros!(false);
+        start!("127.0.0.1:8001" => application);
 
         let mut buffer = Vec::new();
 
         let emoji =
             client_post!(
                 buf: buffer,
-                url["https://127.0.0.1:8001/api/echo"]
+                url["http://127.0.0.1:8001/api/echo/👋"]
                 header["Accept:application/vnd.chat.v1+json"]
                 body["msg=👋"]
             );
